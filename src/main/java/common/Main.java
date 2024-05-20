b7520a1db4fb480c886da9d7c45432bf0b35924c
sooYeon <hks3274@naver.com>
2024-05-20 10:54:29 +0900

    20240520_1054 초기세팅

---
+++ src/main/java/common/Main.java
@@ -0,0 +1,29 @@
+package common;
+
+import java.io.IOException;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+@SuppressWarnings("serial")
+@WebServlet("/Main")
+public class Main extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		/*
+		LoginDAO dao = new LoginDAO();
+		
+		ArrayList<LoginVO> recentVos = dao.getRecentFiveMember();
+		
+		request.setAttribute("recentVos", recentVos);
+		*/
+		
+		String viewPage = "/WEB-INF/main/main.jsp";
+		RequestDispatcher dispatcher = request.getRequestDispatcher(viewPage);
+		dispatcher.forward(request, response);
+	}
+}
---
+++ src/main/webapp/include/bs4.jsp
@@ -0,0 +1,6 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8" pageEncoding="UTF-8"%>
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+
